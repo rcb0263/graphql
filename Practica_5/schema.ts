f5b5293208c341b67223a7d297494cd302692ee5
rcb0263 <149785947+rcb0263@users.noreply.github.com>
2024-12-15 22:11:43 +0100

    Add files via upload

--- Practica_5/schema.ts
+++ Practica_5/schema.ts
@@ -1,16 +1,77 @@
 export const schema = `#graphql
-type Dinosaur {
+
+type Student {
   id: ID!
   name: String!
-  type: String!
+  email: String!
+  enrolledCourses: [String!]!
 }
+
+type Teacher {
+  id: ID!
+  name: String!
+  email: String!
+  coursesTaught: [String!]!
+}
+
+type Course {
+  id: ID!
+  title: String!
+  description: String!
+  teacherId: String!
+  studentIds: [String!]!
+}
+
+type StudentGQ {
+  id: ID!
+  name: String!
+  email: String!
+  enrolledCourses: [Course!]!
+}
+
+type TeacherGQ {
+  id: ID!
+  name: String!
+  email: String!
+  coursesTaught: [Course!]!
+}
+
+type CourseGQ {
+  id: ID!
+  title: String!
+  description: String!
+  teacherId: Teacher!
+  studentIds: [Student!]!
+}
+
 type Query {
-  dinosaurs: [Dinosaur!]!
-  dinosaur(id: ID!): Dinosaur
+  students: [StudentGQ!]!     #Funciona
+  student(id: ID!): StudentGQ     #Funciona
+  teachers: [TeacherGQ!]!    #Funciona
+  teacher(id: ID!): TeacherGQ    #Funciona
+  courses: [CourseGQ!]!    #Funciona
+  course(id: ID!): CourseGQ    #Funciona
+
 }
 
 type Mutation {
-  addDinosaur(name: String!, type: String!): Dinosaur!
-  deleteDinosaur(id: ID!): Dinosaur
+  # Crear entidades 
+  createStudent(name: String!, email: String!) : StudentGQ!    #Funciona
+  createTeacher(name: String!, email: String!) : TeacherGQ!    #Funciona
+  createCourse(title: String!, description: String!, teacherId: ID!): CourseGQ!    #Funciona
+
+  # Actualizar entidades 
+  updateStudent(id: ID!, name: String, email: String): Student    #Funciona
+  updateTeacher(id: ID!, name: String, email: String): Teacher    #Funciona
+  updateCourse(id: ID!, title: String, description: String, teacherId: ID): Course    #Funciona
+
+  # Añadir un estudiante a un curso (matricularlo) 
+  enrollStudentInCourse(studentId: ID!, courseId: ID!): CourseGQ!     #Funciona
+  removeStudentFromCourse(studentId: ID!, courseId: ID!): CourseGQ!     #Funciona
+
+  # Eliminar entidades 
+  deleteStudent(id: ID!): Boolean! 
+  deleteTeacher(id: ID!): Boolean! 
+  deleteCourse(id: ID!): Boolean!
 }
 `;
